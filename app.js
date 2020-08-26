370f8344af894e11deb110d147e0201f71eded25
TheMshary <themshary94@gmail.com>
2020-08-26 17:24:42 +0300

    Code Reviewed

--- app.js
+++ app.js
@@ -4,35 +4,35 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const passport = require("passport");
 
-//Strategies
+// Strategies
 // const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
-//DB
+// DB
 const db = require("./db");
 
-//Routes
+// Routes
 
-//Create Express App Instance
+// Create Express App Instance
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-//Passport
+// Passport
 // app.use(passport.initialize());
 // passport.use(localStrategy);
 // passport.use(jwtStrategy);
 
-//Routers
+// Routers
 
-//Not Found Paths
+// Not Found Paths
 app.use((req, res, next) => {
   const error = new Error("Path Not Found");
   error.status = 404;
   next(error);
 });
 
-//Error Handling Middleware
+// Error Handling Middleware
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json(err.message || "Internal Server Error");
@@ -41,8 +41,8 @@ app.use((err, req, res, next) => {
 const run = async () => {
   try {
     await db.sync({});
-    /*force: true*/
-    /*alter: true : we do it only once*/
+    /* force: true */
+    /* alter: true : we do it only once */
   } catch (error) {
     console.error("run -> error", error);
   }
